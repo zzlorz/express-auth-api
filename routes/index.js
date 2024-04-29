a46a43e0bb58a58427d0df62500e669bcf1a4e8a
zzlorz <zhuzheng.lei@163.com>
2024-04-29 06:34:21 +0000

    提交

--- routes/index.js
+++ routes/index.js
@@ -22,17 +22,22 @@ router.get("/login", async (req, res, next) => {
   }
   try {
     // 生成token
-    const {error, data, count, status, statusText} = await supabase.from("user").select("*").eq('name', name)
+    const {error, data, count, status, statusText} = await supabase.from("user").select("*").eq('name', name);
+    if(error) {
+      res.json({ errcode: 1, msg: "系统错误，登录失败" });
+      return
+    }
+    const {pw: data_pw, id} = data[0];
     if(data.length>0) {
       // 验证密码
       const isPasswordValid = bcrypt.compareSync(
         pw,
-        user.data[0].pw
+        data_pw
       )
       if(isPasswordValid) {
         const token = jwt.sign(
           {
-            id: String(user.data[0].id)}, 
+            id: String(id)}, 
             process.env.JWT_SECRET, 
             { expiresIn: "2h" }
         );
@@ -43,10 +48,10 @@ router.get("/login", async (req, res, next) => {
     }else{
       res.json({ errcode: 0, msg: "用户名或者密码错误" });
     }       
-    return;
+    // return;
   } catch (error) {
     res.send({ errcode: 1, data: error });
-    return;
+    // return;
   }
 });
 /**
@@ -60,7 +65,11 @@ router.get("/register", async (req, res, next) => {
     res.json({ errcode: 1, msg: "用户名或者密码不能为空" });
     return;
   }
-  const {error, data, count, status, statusText} = await supabase.from("user").select("*").eq('name', name)
+  const {error, data, count, status, statusText} = await supabase.from("user").select("*").eq('name', name);
+  if(error) {
+    res.json({ errcode: 1, msg: "系统错误" });
+    return
+  }
   if(data.length>0) {
     res.json({ errcode: 1, msg: "用户名已存在" });
     return;
@@ -86,7 +95,5 @@ module.exports = router;
 SALT_ROUNDS='abc@abc'
 JWT_SECRET='abc@abc'
 SUPABASE_URL='https://dxzmebuimxtfznmcdwht.supabase.co'
-SUPABASE_KEY=`eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
-eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4em1lYnVpbXh0ZnpubWNkd2h0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI3MzMxMTMsImV4cCI6MjAyODMwOTExM30.
-HrTOiN3nsf6EJBcq8nw5ZpO5H23g5OZ8oSN1f-fPq0Q`
+SUPABASE_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4em1lYnVpbXh0ZnpubWNkd2h0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI3MzMxMTMsImV4cCI6MjAyODMwOTExM30.HrTOiN3nsf6EJBcq8nw5ZpO5H23g5OZ8oSN1f-fPq0Q'
 */
